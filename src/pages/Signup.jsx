971a29343e6f0e1c31a0c9b0bda079dd6f59234c
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-02-21 23:50:44 -0700

    added sign in page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,21 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
     <>
       <div className="w-full h-screen">
-        <img alt='' className="hidden sm:block absolute w-full h-full object-cover" src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/6f5fcbd6-993f-4d76-b207-799c937026ae/US-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"/>
+        <img
+          alt=""
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/6f5fcbd6-993f-4d76-b207-799c937026ae/US-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
         <div className="fixed w-full px-4 py-24 z-50">
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign Up</h1>
-              <form className="w-full flex flex-col py-4"> 
-                <input className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email"/>
-                <input className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password"/>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Email"
+                />
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="Password"
+                />
                 <button className="bg-red-600 py-3 my-6 rounded font-bold">
                   Sign Up
                 </button>
+                <div className="flex justify-between items-center text-sm text-gray-600 cursor-pointer">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600">Already Subscribed?</span>{" "}
+                  <Link to="/login" className="cursor-pointer text-gray-200">Sign in</Link>
+                </p>
               </form>
             </div>
           </div>
