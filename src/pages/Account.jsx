34a6435127dc9b005fa2ef28e75ae8e555ffdfaf
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-02-27 16:50:02 -0700

    added subdivs

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account page update</div>
+    <div>Account page update
+      <p>This is going slow</p>
+    </div>
   )
 }
 
