3e5a3db23cbdd8a59d32c20ccab160eb7c75026b
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-03-18 22:32:27 -0600

    1

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Account = () => {
   return (
     <div>Account page update
-      <p>Sick Day last<Test></Test></p>
+      <p>Sick Day last<Test>1</Test></p>
     </div>
   )
 }
