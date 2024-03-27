4c4fec6b9d15084653a488aea108fd35a7baeec1
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-03-27 00:07:01 -0600

    Start ing to build liked  shows section

--- src/componets/SavedShows.jsx
+++ src/componets/SavedShows.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const SavedShows = () => {
   return (
-    <div>SavedShows</div>
+    <>
+    <h2 className='text-white font-bold md:text-xl p-4'>My Shows</h2>
+    <div className='relitive flex items-center group'>
+        <MdChevronLeft 
+        onClick={slideLeft}
+        className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+            {movies.map((item, id) => (
+                <Movie key={id} item={item}/>
+            ))}
+        </div>
+        <MdChevronRight 
+        onClick={slideRight}
+        className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>           
+    </div>
+    </>
   )
 }
 
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div>Account page update
-      <p>Home from training</p>
+    <>
+    <div className='w-full text-white'>
+    <img
+          alt=""
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/6f5fcbd6-993f-4d76-b207-799c937026ae/US-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
+
+        </div>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
     </div>
+    </>
   )
 }
 
