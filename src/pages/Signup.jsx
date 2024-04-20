1cb60150f6b333f36876fab07b074e82e4c2d015
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-04-19 23:50:37 -0600

    Phrasing updates

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -50,7 +50,7 @@ const Signup = () => {
                 <div className="flex justify-between items-center text-sm text-gray-600 cursor-pointer">
                   <p>
                     <input className="mr-2" type="checkbox" />
-                    Remember me please
+                    Remember me
                   </p>
                   <p>Need Help?</p>
                 </div>
